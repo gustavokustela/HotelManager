46316f0e3c7b0f66872d5e1d7e2231bef9550e0a
gabrielquadrado <gabrielquadrado18@gmail.com>
2016-11-22 21:30:30 -0200

    alteracao na estrutura de pastas

--- HotelManager/src/Main.java
+++ HotelManager/src/Main.java
@@ -1,3 +1,26 @@
-public class Main{
-    
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class Main extends Application{
+    public static void main(String[] args) {
+        Application.launch(Main.class, (java.lang.String[])null);
+    }
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        try {
+            StackPane page = (StackPane) FXMLLoader.load(Main.class.getResource("../../JavaFX.fxml"));
+            Scene scene = new Scene(page);
+            primaryStage.setScene(scene);
+            primaryStage.setTitle("FXML is Simple");
+            primaryStage.show();
+        } catch (Exception ex) {
+            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
 }
