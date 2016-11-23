cc19175ae59b8770feee7302c7ba78905c2b0903
Gustavo Silva <gustavo_vsilva@outlook.com>
2016-11-23 21:47:50 -0200

    declaracao de recursos e metodos para view de cadastro de hospedes

--- HotelManager/src/View/CadastroHospedeViewController.java
+++ HotelManager/src/View/CadastroHospedeViewController.java
@@ -1,4 +1,98 @@
 package View;
 
 public class CadastroHospedeViewController {
+	
+	//Declaração de recursos (elementos) disponíveis para a tela de Cadastro de Hospede	
+	@FXML
+	private Pane mainCadastroDeHospedes;
+	
+	@FXML
+	private Label titleCadastroDeHospedes;
+	
+	@FXML
+	private Label lblDadosPessoais;
+	
+	@FXML
+	private Label lblDadosDeContato;
+	
+	@FXML
+	private Label lblDadosDeEndereco;
+	
+	@FXML
+	private Separator sepBotton;
+	
+	@FXML
+	private Separator sepMid;
+	
+	@FXML
+	private Separator sepTop;
+	
+	@FXML
+	private ImageView imgIconCadastroDeHospedes;
+	
+	@FXML
+	private imageView imgCancelBtn;
+	
+	@FXML
+	private imageView imgFinalBtn;
+	
+	@FXML
+	private TextField txtNomeHosp;
+	
+	@FXML
+	private TextField txtRgHosp;	
+	
+	@FXML
+	private TextField txtCpfHosp;
+	
+	@FXML
+	private TextField txtDtNascHosp;
+	
+	@FXML
+	private TextField txtSexoHosp;
+	
+	@FXML
+	private TextField txtTelHosp;
+	
+	@FXML
+	private TextField txtEmailHosp;	
+	
+	@FXML
+	private TextField txtEndLogradouroHosp;
+	
+	@FXML
+	private TextField txtEndNrHosp;
+	
+	@FXML
+	private TextField txtEndBairroHosp;
+	
+	@FXML
+	private TextField txtEndCidadeHosp;
+	
+	@FXML
+	private TextField txtEndBairroHosp;
+	
+	@FXML
+	private TextField txtEndEstadoHosp;
+	
+	@FXML
+	private CheckBox checkBoxIsEstrangeiroHosp;
+	
+	@FXML
+	private Button btnCancelCadHosp;
+	
+	@FXML
+	private Button btnFinalCadHosp;
+	
+	
+	//Declaração de metodos
+	@FXML
+	public void cancelarCadastro(){
+		
+	}
+	
+	@FXML
+	public void finalizarCadastro(){
+		
+	}
 }
