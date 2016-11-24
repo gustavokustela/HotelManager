801baf99851757eab766457a21c38ff6f42facb7
Gustavo Silva <gustavo_vsilva@outlook.com>
2016-11-24 20:45:40 -0200

    insercao de IDs e controller --Tela (menu) de Relatorios--

--- HotelManager/src/View/RelatorioViewController.java
+++ HotelManager/src/View/RelatorioViewController.java
@@ -1,4 +1,55 @@
 package View;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.Pane;
+
 public class RelatorioViewController {
+	
+	//Declaracao de recursos (elementos) disponiveis para a tela de Cadastro de Hospede
+	@FXML
+	private Pane mainRelatorio;
+	
+	@FXML
+	private ImageView imgRelatorioTitle;
+	
+	@FXML
+	private ImageView imgRelatorioHospede;
+	
+	@FXML
+	private ImageView imgRelatorioDespesa;
+	
+	@FXML
+	private ImageView imgRelatorioQuarto;
+	
+	@FXML
+	private Button btnRelHospedes;
+	
+	@FXML
+	private Button btnRelDespesas;
+	
+	@FXML
+	private Button btnRelQuartos;
+	
+	@FXML
+	private Label titleCadastroRelatorio;
+	
+	
+	//Declaracao de metodos para abertura de relatorios
+	//metodo para exibicao de relatorio de hospedes cadastrados
+	public void relatorioHospedes(){
+		
+	}
+	
+	//metodo para exibicao de relatorio de despesas do hospede (apos check-out)
+	public void relatorioDespesas(){
+		
+	}
+	
+	//metodo para exibicao de relatorio de quartos do hotel
+	public void relatorioQuartos(){
+		
+	}
 }
